bc8f367712a98708071ea869071268827ef8bbea
Micaela Medina <medimica@gmail.com>
2023-08-07 16:08:07 -0300

    otro commit

--- src/client/pages/new-game.ts
+++ src/client/pages/new-game.ts
@@ -14,14 +14,29 @@ class NewChatroom extends HTMLElement{
     render(){
         this.innerHTML = `
             <div class="new-chatroom-section">
-                hola
+                <custom-cat-silhouette></custom-cat-silhouette>
+                <div class="new-chatroom-section-items-container">
+                    <h3 class="room-id-reference">ID de la sala:</h3>
+                    <h1 class="id"></h1>
+                    <h3 class="room-id-reference">Compartilo con tu contrincante</h3>
+                </div>
+                <custom-line-img class="bottom-line-img"></custom-line-img>
             </div>
         `
 
         const style = document.createElement("style");
         style.innerHTML = `
-            .new-chatroom-section{}
-
+            .new-chatroom-section{
+                height: 100vh;
+                padding: 0px 7px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                overflow: auto;
+                gap: 35px;
+                background: linear-gradient( to left, #0e123b, #0a325c, #206985 );
+            }
         `;
 
         this.appendChild(style)
