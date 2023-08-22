6030be8f206450b4da3ed1672fbac26e794da565
Micaela Medina <medimica@gmail.com>
2023-08-22 15:51:30 -0300

    arreglando espacio

--- src/client/pages/play-page.ts
+++ src/client/pages/play-page.ts
@@ -9,8 +9,8 @@ class Play extends HTMLElement{
         this.innerHTML = `
             <div class="play-section">
                 <div class="names-container">
-                    <h3 class="opponent-name">Mica: 0</h3>
-                    <h3 class="my-name">Maguis: 0</h3>
+                    <h3 class="opponent-name">Maguis: 0</h3>
+                    <h3 class="my-name">Mica: 0</h3>
                 </div>
                 <div class="cat-hands-container">
                     <custom-cat-paper></custom-cat-paper>
@@ -18,7 +18,7 @@ class Play extends HTMLElement{
                     <custom-cat-scissors></custom-cat-scissors>
                 </div>
                 <div class="room-id-container">
-                    <h3 class="room-text">Sala</h3>
+                    <h3 class="room-text">Sala:</h3>
                     <h2 class="room-id">65439</h2>
                 </div>
             </div>
@@ -31,22 +31,33 @@ class Play extends HTMLElement{
                 display: flex;
                 flex-direction: row;
                 justify-content: space-evenly;
+                padding: 0px 15px;
                 background: linear-gradient(to left, #0e123b, #0a325c, #206985);
             }
 
             .names-container,
             .room-id-container{
+                display: flex;
+                flex-direction: column;
                 padding-top: 30px;
+                gap: 12px;
             }
 
             .opponent-name,
-            .my-name,
             .room-text{
                 margin: 0px;
-                font-size: 23px;
+                font-size: 24px;
                 font-weight: 400;
                 font-family: 'Roboto', sans-serif;
                 color: #ffffff;
+            }
+
+            .my-name{
+                margin: 0px;
+                font-size: 24px;
+                font-weight: 400;
+                font-family: 'Roboto', sans-serif;
+                color: #da70db;
             }
 
             .room-id{
@@ -54,7 +65,7 @@ class Play extends HTMLElement{
                 font-size: 25px;
                 font-weight: 400;
                 font-family: 'Roboto', sans-serif;
-                color: #ffffff;
+                color: #da70db;
             }
 
             .cat-hands-container{
