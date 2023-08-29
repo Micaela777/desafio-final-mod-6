a192bb7911f306f355d35bb15f837e4fb31f793a
Micaela Medina <medimica@gmail.com>
2023-08-29 18:20:20 -0300

    hice visible el componente countdown

--- src/client/index.ts
+++ src/client/index.ts
@@ -10,14 +10,15 @@ import { state } from "./state";
 
 import { initMainTitleComponent } from "./components/main-title";
 import { initHomeSmallTextComponent } from "./components/home-small-text";
-import { initAuthFormComponent } from "./components/auth-form";
 import { initHomeStartButtonComponent } from "./components/home-start-button";
 import { initHomeCatImageComponent } from "./components/home-space-cat";
+import { initAuthFormComponent } from "./components/auth-form";
 import { initChooseOptionButtonComponent } from "./components/choose-option-button";
 import { initCatSilhouetteComponent } from "./components/cat-silhouette";
 import { initLineImgComponent } from "./components/custom-line-img";
 import { initErrorTextComponent } from "./components/error-text";
 import { initEnterTheRoomFormComponent } from "./components/enter-the-room-form";
+import { initCountdownComponent } from "./components/countdown-component";
 import { initCatPaperComponent } from "./components/cat-paper";
 import { initCatRockComponent } from "./components/cat-rock";
 import { initCatScissorsComponent } from "./components/cat-scissors";
@@ -26,14 +27,15 @@ import { initCatScissorsComponent } from "./components/cat-scissors";
 
     initMainTitleComponent();
     initHomeSmallTextComponent();
-    initAuthFormComponent();
     initHomeStartButtonComponent();
     initHomeCatImageComponent();
+    initAuthFormComponent();
     initChooseOptionButtonComponent();
     initEnterTheRoomFormComponent();
     initCatSilhouetteComponent()
     initLineImgComponent();
     initErrorTextComponent();
+    initCountdownComponent()
     initCatPaperComponent();
     initCatRockComponent();
     initCatScissorsComponent();
