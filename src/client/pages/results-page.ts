4ece9c26b75270e3976503f4cc30c79ef7c456fd
Micaela Medina <medimica@gmail.com>
2023-09-01 18:08:13 -0300

    mas cambios en la pagina results

--- src/client/pages/results-page.ts
+++ src/client/pages/results-page.ts
@@ -8,18 +8,18 @@ class Results extends HTMLElement{
       render(){
           this.innerHTML = `
               <div class="results-section">
-                  <div class="results-section-container">
+                  
                       <custom-win-img></custom-win-img>
                       <div class="score-container">
                           <h2 class="score-title">Puntaje</h2>
                           <h4 class="opponent-score">Maguis: 0</h4>
-                          <h4 class="my-score"><span class="my-name">Mica:</span>0</h4>
+                          <h4 class="my-score"><span class="my-name">Mica:</span> 0</h4>
                       </div>
                       <div class="buttons-conatiner">
                           <custom-choose-option-button class="play-again-button">Volver a jugar</custom-choose-option-button>
                           <custom-choose-option-button class="leave-button">Salir</custom-choose-option-button>
                       </div>
-                  </div>
+                  
               </div>
           `
   
@@ -27,21 +27,37 @@ class Results extends HTMLElement{
           style.innerHTML = `
             .results-section{
                 height: 100vh;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                gap: 20px;
                 background: linear-gradient( #070708, 92%, #5cc4bb );
             }
-  
+
             .score-container{
+                height: 145px;
+                width: 229px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                border-radius: 30px;
+                gap: 5px;
                 background-color: #ffffff;
             }
 
             .score-title{
                 margin: 0px;
+                margin-bottom: 10px;
+                text-align: center;
                 font-family: 'Roboto', sans-serif;
             }
 
             .opponent-score,
             .my-score{
                 margin: 0px;
+                margin-left: 28px;
+                font-size: 20px;
                 font-family: 'Roboto', sans-serif;
             }
 
