e5877d5373f5d09670d0af212f2074eb45ce4e6d
Micaela Medina <medimica@gmail.com>
2023-09-18 18:04:26 -0300

    el cambio anterior no funcionó

--- src/server/index.ts
+++ src/server/index.ts
@@ -87,7 +87,7 @@ app.get("/rooms/:roomId", (req, res) => {
 
   app.use(express.static("dist"));
   app.get("*", (req, res) => {
-    res.sendFile(__dirname, "../../dist/index.html" as any)
+    res.sendFile(path.join(__dirname, "../../dist/index.html"))
   })
 
   app.listen(port, () => {
