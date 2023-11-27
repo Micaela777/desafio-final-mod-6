62847a93fd3bf457ad7980f340d3270b99065a7c
Micaela Medina <medimica@gmail.com>
2023-11-27 20:46:29 -0300

    agregando estilos a las manos

--- src/client/pages/playing.ts
+++ src/client/pages/playing.ts
@@ -14,6 +14,151 @@ class Playing extends HTMLElement{
             Router.go('./no-choise');
         };
     }, 1000);*/
+
+        const countdown = this.querySelector(".countdown") as any;
+
+        const bottomPaper = this.querySelector('.bottom-paper') as any;
+        const bottomPaperComponentImg = bottomPaper.shadowRoot.querySelector('.paper-img') as any;
+        bottomPaperComponentImg.style.height = '292px';
+        bottomPaperComponentImg.style.width = '116px';
+
+        const removePaperClick = () => {
+            //state.setMove("tijeras");
+            bottomPaperComponentImg.style.objectPosition = '0px 43px';
+            bottomPaperComponentImg.style.transition = 'all 0.2s ease-out';
+    
+            // Acá se le baja opacidad a los elementos no elegidos
+            bottomScissors.style.opacity = '0.4';
+            bottomRock.style.opacity = '0.4';
+    
+            /*setTimeout(() => {
+                if (state.data.currentGame.computerPlay == "tijeras") {
+                    computerScissorsComponent.style.display = 'flex';
+                } else if (state.data.currentGame.computerPlay == "piedra") {
+                    computerRockComponent.style.display = 'flex';
+                } else if (state.data.currentGame.computerPlay == "papel") {
+                    computerPaperComponent.style.display = 'flex';
+                };
+    
+                countdownComponent.style.display = 'none';
+                rockComponent.style.display = 'none';
+                paperComponent.style.display = 'none';
+    
+                bottomPaperComponentImg.style.height = '279px';
+                bottomPaperComponentImg.style.width = '87px';
+            }, 800);
+    
+            clearInterval(intervalId);
+            setTimeout(() => {
+                params.goTo('/results');
+            }, 2500);*/
+            bottomPaperComponentImg.removeEventListener('click', removePaperClick);
+            bottomPaper.style.cursor = 'default';
+            bottomPaper.style.transform = 'unset';
+            bottomScissorsComponentImg.removeEventListener("click", removeScissorsClick);
+            bottomScissors.style.cursor = 'default';
+            bottomScissors.style.transform = 'unset';
+            bottomRockComponentImg.removeEventListener('click', removeRockClick);
+            bottomRock.style.cursor = 'default';
+            bottomRock.style.transform = 'unset';
+        }
+        bottomPaperComponentImg.addEventListener('click', removePaperClick);
+
+
+        const bottomRock = this.querySelector('.bottom-rock') as any;
+        const bottomRockComponentImg = bottomRock.shadowRoot.querySelector('.rock-img') as any;
+        bottomRockComponentImg.style.height = '281px';
+        bottomRockComponentImg.style.width = '96px';
+
+        const removeRockClick = () => {
+            //state.setMove("tijeras");
+            bottomRockComponentImg.style.objectPosition = '0px 43px';
+            bottomRockComponentImg.style.transition = 'all 0.2s ease-out';
+    
+            // Acá se le baja opacidad a los elementos no elegidos
+            bottomScissors.style.opacity = '0.4';
+            bottomPaper.style.opacity = '0.4';
+    
+            /*setTimeout(() => {
+                if (state.data.currentGame.computerPlay == "tijeras") {
+                    computerScissorsComponent.style.display = 'flex';
+                } else if (state.data.currentGame.computerPlay == "piedra") {
+                    computerRockComponent.style.display = 'flex';
+                } else if (state.data.currentGame.computerPlay == "papel") {
+                    computerPaperComponent.style.display = 'flex';
+                };
+    
+                countdownComponent.style.display = 'none';
+                rockComponent.style.display = 'none';
+                paperComponent.style.display = 'none';
+    
+                bottomRockComponentImg.style.height = '279px';
+                bottomRockComponentImg.style.width = '87px';
+            }, 800);
+    
+            clearInterval(intervalId);
+            setTimeout(() => {
+                params.goTo('/results');
+            }, 2500);*/
+            bottomRockComponentImg.removeEventListener('click', removeRockClick);
+            bottomRock.style.cursor = 'default';
+            bottomRock.style.transform = 'unset';
+            bottomScissorsComponentImg.removeEventListener("click", removeScissorsClick);
+            bottomScissors.style.cursor = 'default';
+            bottomScissors.style.transform = 'unset';
+            bottomPaperComponentImg.removeEventListener('click', removePaperClick);
+            bottomPaper.style.cursor = 'default';
+            bottomPaper.style.transform = 'unset';
+        }
+        bottomRockComponentImg.addEventListener('click', removeRockClick);
+    
+
+        const bottomScissors = this.querySelector('.bottom-scissors') as any;
+        const bottomScissorsComponentImg = bottomScissors.shadowRoot.querySelector('.scissors-img') as any;
+        bottomScissorsComponentImg.style.height = '281px';
+        bottomScissorsComponentImg.style.width = '96px';
+
+        const removeScissorsClick = () => {
+            //state.setMove("tijeras");
+            bottomScissorsComponentImg.style.objectPosition = '0px 43px';
+            bottomScissorsComponentImg.style.transition = 'all 0.2s ease-out';
+    
+            // Acá se le baja opacidad a los elementos no elegidos
+            bottomRock.style.opacity = '0.4';
+            bottomPaper.style.opacity = '0.4';
+    
+            /*setTimeout(() => {
+                if (state.data.currentGame.computerPlay == "tijeras") {
+                    computerScissorsComponent.style.display = 'flex';
+                } else if (state.data.currentGame.computerPlay == "piedra") {
+                    computerRockComponent.style.display = 'flex';
+                } else if (state.data.currentGame.computerPlay == "papel") {
+                    computerPaperComponent.style.display = 'flex';
+                };
+    
+                countdownComponent.style.display = 'none';
+                rockComponent.style.display = 'none';
+                paperComponent.style.display = 'none';
+    
+                bottomScissorsComponentImg.style.height = '279px';
+                bottomScissorsComponentImg.style.width = '87px';
+            }, 800);
+    
+            clearInterval(intervalId);
+            setTimeout(() => {
+                params.goTo('/results');
+            }, 2500);*/
+            bottomScissorsComponentImg.removeEventListener('click', removeScissorsClick);
+            bottomScissors.style.cursor = 'default';
+            bottomScissors.style.transform = 'unset';
+            bottomRockComponentImg.removeEventListener("click", removeRockClick);
+            bottomRock.style.cursor = 'default';
+            bottomRock.style.transform = 'unset';
+            bottomPaperComponentImg.removeEventListener('click', removePaperClick);
+            bottomPaper.style.cursor = 'default';
+            bottomPaper.style.transform = 'unset';
+        }
+        bottomScissorsComponentImg.addEventListener('click', removeScissorsClick);
     
     };
 
@@ -91,26 +236,6 @@ class Playing extends HTMLElement{
         `;
 
         this.appendChild(style)
-
     };
-
-    addListeners(){
-        const countdown = this.querySelector(".countdown")
-
-        const bottomPaper = this.querySelector('.paper') as any;
-        const bottomPaperComponentImg = bottomPaper.shadowRoot.querySelector('.paper-img') as any;
-        bottomPaperComponentImg.style.height = '292px';
-        bottomPaperComponentImg.style.width = '116px';
-
-        const bottomRock = this.querySelector('.rock') as any;
-        const bottomRockComponentImg = bottomRock.shadowRoot.querySelector('.rock-img') as any;
-        bottomRockComponentImg.style.height = '281px';
-        bottomRockComponentImg.style.width = '96px';
-
-        const bottomScissors = this.querySelector('.scissors') as any;
-        const bottomScissorsComponentImg = bottomScissors.shadowRoot.querySelector('.scissors-img') as any;
-        bottomScissorsComponentImg.style.height = '281px';
-        bottomScissorsComponentImg.style.width = '96px';
-    }
 };
 customElements.define('playing-page', Playing);
