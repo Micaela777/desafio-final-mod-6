03fc045278ee803f4ff50e48f95cce148acc2bc7
Micaela Medina <88109114+Micaela777@users.noreply.github.com>
2024-01-05 11:16:08 -0300

    Update state.ts

--- src/client/state.ts
+++ src/client/state.ts
@@ -1,7 +1,7 @@
 import { Router } from "@vaadin/router";
 import { rtdb } from "./rtdb";
 
-const API_BASE_URL = "http://localhost:3000"
+const API_BASE_URL = "https://ppt-online.onrender.com"
 
 type Jugada = "piedra" | "papel" | "tijeras";
 
